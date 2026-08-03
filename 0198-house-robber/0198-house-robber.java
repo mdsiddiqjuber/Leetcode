35cca8970f843d202fd2b2d277b459bfc1e5291c
mdsiddiqjuber <mdsiddiqjuber@gmail.com>
2026-08-03 23:31:35 +0530

    Time: 0 ms (100%), Space: 42.5 MB (95.17%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,17 +1,24 @@
 class Solution {
-    int[] t;
-    int solve(int[] nums, int idx) {
-        if(idx == 0) return nums[idx];
-        if(idx < 0) return 0;
-        if(t[idx] != -1) return t[idx];
-        int pick = nums[idx] + solve(nums, idx - 2);
-        int notPick = 0 + solve(nums, idx - 1);
-        return t[idx] = Math.max(pick, notPick);
-    }
+    // int[] t;
+    // int solve(int[] nums, int idx) {
+    //     if(idx == 0) return nums[idx];
+    //     if(idx < 0) return 0;
+    //     if(t[idx] != -1) return t[idx];
+    //     int pick = nums[idx] + solve(nums, idx - 2);
+    //     int notPick = 0 + solve(nums, idx - 1);
+    //     return t[idx] = Math.max(pick, notPick);
+    // }
     public int rob(int[] nums) {
         int n = nums.length;
-        t = new int[n];
+        int[] t = new int[n];
         Arrays.fill(t, -1);
-        return solve(nums, n - 1);
+        t[0] = nums[0];
+        for(int i = 1; i < n; i++) {
+            int pick = nums[i];
+            if(i > 1) pick += t[i - 2];
+            int notPick = t[i - 1];
+            t[i] = Math.max(pick, notPick);
+        }
+        return t[n - 1];
     }
 }
